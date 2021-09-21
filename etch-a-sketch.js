ad73dae43e4d6a3c736a3c71404d369474b1e206
Nicolas Trucco <ntrucco@gmail.com>
2021-09-21 14:53:29 -0300

    uploading files

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,34 @@
-<script src="rock-paper-scissors-dom.js"></script>
+//Crea los divs:
+function divCreator(size){
+    for (i=1; i<=size*size; i++){
+            var d=document.createElement("div");
+                container.appendChild(d);
+                d.className="border";
+                d.addEventListener('mouseover', colorBackground);
+    }
+}
+// Cambia el color del background
+function colorBackground (){
+    const elements = document.getElementsByClassName('border'); // get all elements
+    this.style.backgroundColor = `blue`;
+}
+// Erase:
+const buttonClear = document.querySelector('#erase')
+const gridContainer = document.querySelector('#container')
+
+function erase(){
+    let wholeGrid = gridContainer.querySelectorAll('div')
+    wholeGrid.forEach(oneGrid => oneGrid.style.backgroundColor = '#fff')
+};
+
+buttonClear.addEventListener('click', erase)
+
+
+
+const blackBackground = document.querySelector('#black')
+
+
+
+divCreator(16);
+
+
